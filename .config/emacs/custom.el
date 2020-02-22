7c28cd31e1cd66d9c7b1b7290d5e0f670ab46b2d
Jacob <jacob.hilker2@gmail.com>
2020-02-21 19:53:14 -0500

    initial commit

---
+++ .config/emacs/custom.el
@@ -0,0 +1,69 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#839496" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#fdf6e3"))
+ '(beacon-color "#d33682")
+ '(custom-safe-themes
+   (quote
+    ("0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "e1ecb0536abec692b5a5e845067d75273fe36f24d01210bf0aa5842f2a7e029f" "d5f8099d98174116cba9912fe2a0c3196a7cd405d12fa6b9375c55fc510988b5" "845103fcb9b091b0958171653a4413ccfad35552bc39697d448941bcbe5a660d" "777a3a89c0b7436e37f6fa8f350cbbff80bcc1255f0c16ab7c1e82041b06fccd" "1c8171893a9a0ce55cb7706766e57707787962e43330d7b0b6b0754ed5283cda" "669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" "285efd6352377e0e3b68c71ab12c43d2b72072f64d436584f9159a58c4ff545a" "229c5cf9c9bd4012be621d271320036c69a14758f70e60385e87880b46d60780" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(exec-path
+   (quote
+    ("/home/jhilker/.local/bin" "/home/jhilker/bin" "/usr/local/sbin" "/usr/local/bin" "/usr/sbin" "/usr/bin" "/sbin" "/bin" "/usr/games" "/usr/local/games" "/snap/bin
+" "/usr/lib/x86_64-linux-gnu/emacs/26.3/x86_64-linux-gnu" "/home/jhilker/anaconda3/bin/")))
+ '(fci-rule-color "#073642")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(frame-background-mode (quote dark))
+ '(imenu-list-position (quote right))
+ '(inhibit-startup-screen t)
+ '(ivy-mode t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#073642" "#268bd2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#073642" "#859900"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#073642" "#56697A"))
+ '(jedi:complete-on-dot t)
+ '(objed-cursor-color "#dc322f")
+ '(org-agenda-files (quote ("~/Dropbox/notes/spring-2020.org")))
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
+ '(org-journal-dir "~/Dropbox/org/bullet/")
+ '(org-journal-timestamp-format "%a, %d %b %Y" t)
+ '(package-selected-packages
+   (quote
+    (vimrc-mode wordgen smart-mode-line csv org-make-toc nav neotree slack toc-org org-super-agenda org-bullets bbcode-mode elpy auto-complete counsel ivy decide wordsmith-mode writeroom-mode writegood-mode wc-mode doom-modeline solarized-theme color-theme-solarized doom-themes restart-emacs yasnippet unfill general dashboard projectile all-the-icons magit which-key imenu-list evil-collection evil undo-tree use-package)))
+ '(pdf-view-midnight-colors (cons "#839496" "#002b36"))
+ '(rustic-ansi-faces
+   ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(solarized-underline nil)
+ '(tab-width 4)
+ '(temporary-file-directory "/tmp/")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#cb4b16")
+     (60 . "#b58900")
+     (80 . "#859900")
+     (100 . "#2aa198")
+     (120 . "#268bd2")
+     (140 . "#d33682")
+     (160 . "#6c71c4")
+     (180 . "#dc322f")
+     (200 . "#cb4b16")
+     (220 . "#b58900")
+     (240 . "#859900")
+     (260 . "#2aa198")
+     (280 . "#268bd2")
+     (300 . "#d33682")
+     (320 . "#6c71c4")
+     (340 . "#dc322f")
+     (360 . "#cb4b16"))))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal)))))
