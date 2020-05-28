51420d348692f7872de7ef9e47d104252997bb6f
Jacob Hilker <jacob.hilker2@gmail.com>
2020-05-28 13:19:14 -0400

    added metric units to polybar weather script

--- .config/polybar/scripts/weather/wx.sh
+++ .config/polybar/scripts/weather/wx.sh
@@ -1,2 +1,9 @@
-#!/bin/bash 
- curl -sL 'https://wttr.in?format=1'
+#!/bin/sh 
+curl -sL 'https://wttr.in/?format=%t%20%C' ## for the current temp and condition
+# curl -sL 'https://wttr.in?format=1' ## for the weather symbol and the current temp
+# curl -sL 'https://wttr.in/?format=%t' ## for just the current temp
+
+
+# curl -sL 'https://wttr.in/?format=%t%20%C' ## for the current temp in Celsius and condition
+# curl -sL 'https://wttr.in?format=1' ## for the weather symbol and the current temp in Celsius
+# curl -sL 'https://wttr.in/?format=%t' ## for just the current temp in Celsius
