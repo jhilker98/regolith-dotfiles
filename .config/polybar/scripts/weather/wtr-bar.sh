4ee6570cf050350e4b2c1b4d1864e2c484754012
Jacob Hilker <jacob.hilker2@gmail.com>
2020-06-17 13:57:36 -0400

    Moved keyboard before date, removed condition fr wtr

--- .config/polybar/scripts/weather/wtr-bar.sh
+++ .config/polybar/scripts/weather/wtr-bar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
-curl -sL 'https://wttr.in/?format=%t%20%C' ## for the current temp and condition
+#curl -sL 'https://wttr.in/?format=%t%20%C' ## for the current temp and condition
 # curl -sL 'https://wttr.in?format=1' ## for the weather symbol and the current temp
-# curl -sL 'https://wttr.in/?format=%t' ## for just the current temp
+curl -sL 'https://wttr.in/?format=%t' ## for just the current temp
 
 
 # curl -sL 'https://wttr.in/?format=%t%20%C' ## for the current temp in Celsius and condition
