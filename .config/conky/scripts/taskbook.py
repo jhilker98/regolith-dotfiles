00a86627d5f9708f4e3c761fda7f7a5bb1c4ed2b
Jacob Hilker <jacob.hilker2@gmail.com>
2020-06-27 12:01:46 -0400

    got color formatting for text done

--- .config/conky/scripts/taskbook.py
+++ .config/conky/scripts/taskbook.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
 import json
 import os
+from datetime import datetime, timedelta
 
 path = os.environ['HOME'] + '/Dropbox/.taskbook/storage/storage.json'
 file = open(path, 'r')
 data = json.loads(file.read())
 
 boards = []
-
-boards.extend(
-    [v['boards'][0] for _, v in data.items() if v['boards'][0] not in boards])
+boards.extend([v['boards'][0] for _, v in data.items() if v['boards'][0] not in boards])
 boards = sorted(set(boards))
 for board in boards:
-    print(f'\n{board}'.replace("@",""), )
+    print('${color4}' + f'\n{board}'.replace('@','') + '${color}')
     for _, v in data.items():
+        timestamp = int(v['_timestamp']) / 1000
+        date = datetime.fromtimestamp(timestamp).strftime('%Y%m%d')
+        today = datetime.now().strftime('%Y%m%d')
+        days = int(today) - int(date)
+        days_ago = (
+            '${color8}' + f'{str(timedelta(days=days).days)}d' + '${color}'
+            if days > 0
+            else '${color8}today${color}'
+        )
         if v['boards'][0] == board:
-            if v['_isTask']:
-                is_complete = ['   ' if v['isComplete'] else '   ' if v['inProgress']else '   ']
-                print(f'{is_complete[0]} {v["description"]}')
+            is_starred = '${color3}六${color}' if v['isStarred'] else ''
+            print(
+                f'   {v["_id"]}. {"${color2} ${color} " if v["isComplete"] else "${color1} ${color} "} {v["description"]} {days_ago} {is_starred}'
+                if v["_isTask"]
+                else f'{v["_id"]}. '
+                + "${color6} ${color} "
+                + f'{v["description"]} {days_ago} {is_starred}'
+            )
+file.close()
+
