bc7d5ba551e2b130a0c0d787a7177f37b0778884
Jacob Hilker <jacob.hilker2@gmail.com>
2020-06-27 23:35:29 -0400

    fixed todo to only show in progress and done

--- .config/conky/scripts/taskbook.py
+++ .config/conky/scripts/taskbook.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import json
 import os
 from datetime import datetime, timedelta
@@ -10,26 +11,39 @@ data = json.loads(file.read())
 boards = []
 boards.extend([v['boards'][0] for _, v in data.items() if v['boards'][0] not in boards])
 boards = sorted(set(boards))
-for board in boards:
-    print('${color9}' + f'\n{board}'.replace('@','') + '${color}')
-    for _, v in data.items():
-        timestamp = int(v['_timestamp']) / 1000
-        date = datetime.fromtimestamp(timestamp).strftime('%Y%m%d')
-        today = datetime.now().strftime('%Y%m%d')
-        days = int(today) - int(date)
-        days_ago = (
-            '${color8}' + f'{str(timedelta(days=days).days)}d' + '${color}'
+    #print('${color9}' + f'\n{board}'.replace('@','') + '${color}')
+for _, v in data.items():
+    timestamp = int(v['_timestamp']) / 1000
+    date = datetime.fromtimestamp(timestamp).strftime('%Y%m%d')
+    today = datetime.now().strftime('%Y%m%d')
+    days = int(today) - int(date)
+    days_ago = (
+            '${color3}' + f'{str(timedelta(days=days).days)}d' + '${color}'
             if days > 0
-            else '${color8}today${color}'
-        )
-        if v['boards'][0] == board:
-            is_starred = '${color3}六${color}' if v['isStarred'] else ''
-            print(
-                f'   {v["_id"]}. {"${color2} ${color} " if v["isComplete"] else "${color1} ${color} "} {v["description"]} {days_ago} {is_starred}'
-                if v["_isTask"]
-                else f'{v["_id"]}. '
-                + "${color6} ${color} "
-                + f'{v["description"]} {days_ago} {is_starred}'
+            else '${color3}today${color}'
             )
-file.close()
 
+for board in boards:
+    if v['boards'][0] == board:
+        is_starred = '${color3}六${color}' if v['isStarred'] else ''
+        #print('${color9}' + f'\n{board}'.replace('@','') + '${color}')
+
+
+for _, v in data.items():
+    if not v['isComplete']:
+        if v["boards"][0] != board:
+        
+            board = v["boards"][0]
+            print('${color9}' + f'\n{board}'.replace('@','') + '${color}')
+        #board = v["boards"][0]
+    
+        #print("v boards 0 is {} and board is {}".format(v['boards'][0],board))
+        if not v['inProgress']:
+            check = '${color1}' + '' + '${color}'
+            print(f'  {v["_id"]}. {check}  {v["description"]} {days_ago} {is_starred}')
+        else:
+            ellips = '${color7}' + '' + '${color}'
+            print(f'  {v["_id"]}. {ellips}  {v["description"]} {days_ago} {is_starred}')
+    else:
+        pass
+file.close()
