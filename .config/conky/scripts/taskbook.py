b0fc0f3b4d33c6f3c2d68164881891ffe9e5cf0d
Jacob Hilker <jacob.hilker2@gmail.com>
2020-06-28 19:57:00 -0400

    got image inline, limited to 2 tasks

--- .config/conky/scripts/taskbook.py
+++ .config/conky/scripts/taskbook.py
@@ -8,42 +8,53 @@ path = os.environ['HOME'] + '/Dropbox/.taskbook/storage/storage.json'
 file = open(path, 'r')
 data = json.loads(file.read())
 
+
+def time_ago(date):
+    date_fmt = '%Y%m%d %H%M%S'
+    date = datetime.strptime(date.strftime(date_fmt), date_fmt)
+    today = datetime.strptime(datetime.now().strftime(date_fmt), date_fmt)
+    time_ago = today - date
+    days, hours = time_ago.days, time_ago.seconds // 3600
+    return days, hours
+
+
 boards = []
 boards.extend([v['boards'][0] for _, v in data.items() if v['boards'][0] not in boards])
 boards = sorted(set(boards))
-    #print('${color9}' + f'\n{board}'.replace('@','') + '${color}')
-for _, v in data.items():
-    timestamp = int(v['_timestamp']) / 1000
-    date = datetime.fromtimestamp(timestamp).strftime('%Y%m%d')
-    today = datetime.now().strftime('%Y%m%d')
-    days = int(today) - int(date)
-    days_ago = (
-            '${color3}' + f'{str(timedelta(days=days).days)}d' + '${color}'
-            if days > 0
-            else '${color3}today${color}'
-            )
 
+boards_count = 0
+tasks_count = 0
 for board in boards:
-    if v['boards'][0] == board:
-        is_starred = '${color3}六${color}' if v['isStarred'] else ''
-        #print('${color9}' + f'\n{board}'.replace('@','') + '${color}')
-
-
-for _, v in data.items():
-    if not v['isComplete']:
-        if v["boards"][0] != board:
-        
-            board = v["boards"][0]
-            print('${font Iosevka:bold:italic:size=12}' + '${color9}' + f'\n{board}'.replace('@','') + '${color}' + '${font}')
-        #board = v["boards"][0]
-    
-        #print("v boards 0 is {} and board is {}".format(v['boards'][0],board))
-        if not v['inProgress']:
-            check = '${color1}' + '' + '${color}'
-            print(f'  {v["_id"]}. {check}  {v["description"]} {days_ago} {is_starred}')
-        else:
-            ellips = '${color4}' + '' + '${color}'
-            print(f'  {v["_id"]}. {ellips}  {v["description"]} {days_ago} {is_starred}')
-    else:
-        pass
+    tasks_count = 0
+    if boards_count < 4:
+        print('${font Iosevka:bold:italic:size=12}' + '${color9}' + f'\n{board}'.replace('@','') + '${color}' + '${font}')
+        boards_count += 1
+        for _, v in data.items():
+            date = datetime.fromtimestamp(int(v['_timestamp']) / 1000)
+            days, hours = time_ago(date)
+            days_ago = (
+                    '${color3}' + f'{days}d {hours}h' + '${color}'
+                    if days > 0
+                    else '${color3}today${color}'
+                    )
+            if tasks_count < 2:
+                if v['boards'][0] == board:
+                    is_starred = '${color3}六${color}' if v['isStarred'] else ''
+                    if v['_isTask']:
+                        if not v['isComplete']:
+                            if not v['inProgress']:
+                                print(
+                                        f'   {v["_id"]}. '
+                                        + '${color1}${color}  '
+                                        + f'{v["description"]} {days_ago} {is_starred}'
+                                        )
+                            if v['inProgress']:
+                                print(
+                                        f'   {v["_id"]}. '
+                                        + '${color4}${color}  '
+                                        + f'{v["description"]} {days_ago} {is_starred}'
+                                            )
+                            tasks_count += 1
 file.close()
+
+
