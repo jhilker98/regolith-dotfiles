cfb95b88a5bb9af4c3831a9a15a95c5f1f53ac17
Jacob Hilker <jacob.hilker2@gmail.com>
2020-07-07 18:30:21 -0400

    added quotes and  agenda

---
+++ .config/conky/scripts/agenda.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$HOME/anaconda3/bin/gcalcli --conky agenda | sed "s/{color yellow}/{color3}/g"
+
+
+
--- .config/conky/scripts/quotes.py
+++ .config/conky/scripts/quotes.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
+import json
+import os
+import random
+import textwrap
 
- 
+path = os.environ['HOME'] + '/.config/conky/scripts/quotes.json'
+file = open(path, 'r')
+data = json.loads(file.read())
+quote = random.choice(data)
+author = quote['author']
+print('${font} '
+    + f'{textwrap.fill(quote["quote"], 52)} '
+    + '${font}'
+    + '\n   - ${font Iosevka Nerd Font:italic:size=11}'
+    + '${color7}'
+    + f'{author}'
+    + '${font}'
+    + '${color}'
+)
