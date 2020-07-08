07066c6fe9727a0adaca10f5ebd38b3d4b7a4931
Jacob Hilker <jacob.hilker2@gmail.com>
2020-07-07 23:10:52 -0400

    redid agenda for 2 days, todo positioning

--- .config/conky/scripts/agenda.sh
+++ .config/conky/scripts/agenda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ $($HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted | sed -n 2p) != "No Events Found..."  ]]; then
-  $HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted | sed "s/{color yellow}/{color3}/g" | sed "s/\([0-9]\+:[0-9]\+\)/\${color7}\1\${color}/g"
+if [[ $($HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted "$(date)" "$(date --date='2 days')"| sed -n 2p) != "No Events Found..."  ]]; then
+  $HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted  "$(date)" "$(date --date='2 days')" | sed "s/{color yellow}/{color3}/g" | sed "s/\([0-9]\+:[0-9]\+\)/\${color7}\1\${color}/g"
 else
 #  $HOME/anaconda3/bin/gcalcli --conky agenda | sed "s/{color yellow}/{color8}/g"
 echo -e "\nNo events coming up.\n\n"
