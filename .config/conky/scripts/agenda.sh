cc1dc6b6df95a1d88ddbd687b859e2da78f624ed
Jacob Hilker <jacob.hilker2@gmail.com>
2020-07-08 01:00:22 -0400

    redid agenda to show 8, added nano to the mix

--- .config/conky/scripts/agenda.sh
+++ .config/conky/scripts/agenda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [[ $($HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted "$(date)" "$(date --date='2 days')"| sed -n 2p) != "No Events Found..."  ]]; then
-  $HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted  "$(date)" "$(date --date='2 days')" | sed "s/{color yellow}/{color3}/g" | sed "s/\([0-9]\+:[0-9]\+\)/\${color7}\1\${color}/g"
+if [[ $($HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted | sed -n 2p) != "No Events Found..."  ]]; then
+  $HOME/anaconda3/bin/gcalcli --conky agenda --military --nostarted | sed "s/{color yellow}/{color3}/g" | sed "s/\([0-9]\+:[0-9]\+\)/\${color7}\1\${color}/g" | sed -n 1,8p
+  echo -e -n '\n'
 else
 #  $HOME/anaconda3/bin/gcalcli --conky agenda | sed "s/{color yellow}/{color8}/g"
 echo -e "\nNo events coming up.\n\n"
