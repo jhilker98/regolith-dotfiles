7ce26db9d8477c364ba50c2b13af56099f8d4238
Jacob Hilker <jacob.hilker2@gmail.com>
2020-07-15 20:56:22 -0400

    redid configs for conky, added width to alacritty and changed picom radius

--- .config/conky/scripts/todotoggle.sh
+++ .config/conky/scripts/todotoggle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-CONKY_PID=$(ps aux | grep "[/]usr/bin/conky -q -c /home/jhilker/.config/conky/todo.conf" | awk '{print $2}')
+CONKY_PID=$(ps aux | grep "[/]usr/bin/conky -y -25 -q -c /home/jhilker/.config/conky/todo.conf" | awk '{print $2}')
 
 if [[ -z $CONKY_PID ]]; then
- /usr/bin/conky -q -c /home/jhilker/.config/conky/todo.conf
+ /usr/bin/conky -y -25 -q -c /home/jhilker/.config/conky/todo.conf 
 else
   kill $CONKY_PID
 fi
