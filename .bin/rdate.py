9ed146fcb6cd77d1ca391bfc77e73e65abc53231
Jacob Hilker <jacob.hilker2@gmail.com>
2020-07-26 19:02:04 -0400

    redid aliases and rdate

--- .bin/rdate.py
+++ .bin/rdate.py
@@ -4,7 +4,6 @@ from pprint import pprint
 import argparse
 parser = argparse.ArgumentParser(description="Generates a random date")
 parser.add_argument("-y", "--year", help="Generate a date in a particular year", action='store', dest='year', default='') 
-
 parser.add_argument("-n", "--number", help="Generate a certain number of dates", action='store', dest='number', default=1) 
 args = parser.parse_args()
 
@@ -27,8 +26,8 @@ number = int(vars(args)['number'])
 for num in range(number):
     month = r.choice(list(dates.keys()))
     day = r.randint(1,dates[month])
+
     #print(arg, getattr(args, arg)) 
     print(f'{day:02} {month} {year}')
 #print(r.randint(1,))
-
 
