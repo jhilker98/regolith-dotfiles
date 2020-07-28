b9816fdcb605e2882aba7705c74305650ce30dfd
Jacob Hilker <jacob.hilker2@gmail.com>
2020-07-28 18:33:40 -0400

    redid newsalert

--- .bin/newsalert.sh
+++ .bin/newsalert.sh
@@ -2,9 +2,18 @@
 
 NEWS=$(newsboat -x reload print-unread)
 NUM=$(newsboat -x reload print-unread | awk '{print $1}')
-if [ $NUM = 0 ]; then
-  notify-send "New Articles" "No new articles"
-else
+
+## Don't send if no new articles
+if [ $NUM != 0 ] && [ $NUM != 1 ]; then
   notify-send "New Articles" "$NEWS"
+elif [ $NUM = 1 ]; then
+  notify-send "New Articles" "$NUM unread article"
 fi
-#echo $NEWS
+
+## Send "No new articles" if no new articles
+#if [ $NUM = 0 ]; then
+#  notify-send "New Articles" "No new articles"
+#else
+#  notify-send "New Articles" "$NEWS"
+#fi
+
