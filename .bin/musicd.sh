88d15c0c1ae5f7261215801cfd4cb34a571613f6
Jacob Hilker <jacob.hilker2@gmail.com>
2020-08-16 10:55:22 -0400

    redid musicd

--- .bin/musicd.sh
+++ .bin/musicd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Get the song that is currently played and store it to a variable
 song="$(playerctl metadata title) - $(playerctl metadata artist)"
+player=$(playerctl -l | awk '{print $1}')
 # Stays on loop while the script is active
 while "true"; do
 	# Check if the current song is equal to the one that was playing before (song)
@@ -8,7 +9,9 @@ while "true"; do
 		# If not equal, assign the new song to the variable to be checking again correctly
     song="$(playerctl metadata title) - $(playerctl metadata artist)"
 		# And send the notification
-		notify-send "Current Song" "$song"
+    notify-send "Current Song" "$song" -a $(playerctl -l | awk '{print $1}')
 	# Change "1" to any value in seconds you want
 	sleep 1
+  fi
 done
+
