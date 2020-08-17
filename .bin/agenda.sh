e4758d062d9e43aea06e6749705521995a022a4a
Jacob Hilker <jacob.hilker2@gmail.com>
2020-08-17 00:29:01 -0400

    fixed some of date formatting

--- .bin/agenda.sh
+++ .bin/agenda.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 #echo "Hello World"
-
+time=$(calcurse -r9 --format-recur-apt=' - %S -> %E: %m\n' | grep -o '\([0-9][0-9]:[0-9][0-9]\)' | sed "s/\([0-9][0-9]:[0-9][0-9] \)/\1\n/g" | head -n1)
+#echo $time | sed "s/ /\n/g" | head -n1
 aptformat="  - %S -> %E: %m\n"
 
-#apt=$(calcurse -r10 --format-recur-apt="$aptformat")
-apt=$(calcurse -r10 --format-recur-apt=' - %S -> %E: %m\n' | grep ': \w.*$' | sed 's/^.*: //g' | xargs)
-
+#apt=$(calcurse -r9 --format-recur-apt="$aptformat")
+apt=$(calcurse -r9 --format-recur-apt="$aptformat" | grep ': \w.*$' | sed 's/^.*: //g' | xargs)
+time=`echo $time | sed "s/ /\n/g"`
+echo $time
 #notify-send "Appointment" "$apt" -a "calcurse"
-echo $apt
