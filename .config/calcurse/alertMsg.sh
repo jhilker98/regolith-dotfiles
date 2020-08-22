1e4ea1b882fa750091c011b3b5e0558f098b4b4a
Jacob Hilker <jacob.hilker2@gmail.com>
2020-08-21 22:19:36 -0400

    added sound to notify-send

--- .config/calcurse/alertMsg.sh
+++ .config/calcurse/alertMsg.sh
@@ -5,5 +5,8 @@ evt=$(calcurse -a --format-apt='%S - %m\n' | sed -n 2p)
 email=$(printf '%b\n   • %b\n' "$msg" "$evt")
 
 notify-send "Event Soon" "$evt" -a "calcurse" -u critical;
+for sound in $(seq 1); 
+  do $(aplay -q ~/Music/alerts/alert.wav) ; 
+done
 printf '%s\n' "$email" | neomutt -s "Event Soon" $EMAIL
 
