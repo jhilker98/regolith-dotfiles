6897037f3cdb43b24c214c9da4a8d62561e71644
Jacob Hilker <jacob.hilker2@gmail.com>
2020-08-22 23:27:47 -0400

    redid alertMsg, redid conf

--- .config/calcurse/alertMsg.sh
+++ .config/calcurse/alertMsg.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-
-msg="You have an event coming up in an hour."
+agenda=$(calcurse -a --format-apt='%S - %m\n' | awk 'NR>1' | sed 's/^/   • /g')
+agenda="$agenda\n" | sed '/^\s*$/d'
 evt=$(calcurse -a --format-apt='%S - %m\n' | sed -n 2p)
-email=$(printf '%b\n   • %b\n' "$msg" "$evt")
-
+len=$(echo $agenda | wc -l)
+email='You have events coming up in an hour.\n'
+email="$email$agenda"
 notify-send "Event Soon" "$evt" -a "calcurse" -u critical;
 for sound in $(seq 1); 
   do $(aplay -q ~/Music/alerts/alert.wav) ; 
 done
-printf '%s\n' "$email" | neomutt -s "Event Soon" $EMAIL
+echo "$email" | neomutt -s "Event Soon" $EMAIL
 
