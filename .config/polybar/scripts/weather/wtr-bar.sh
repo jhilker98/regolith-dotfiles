5263508ab20f376b277cea2c62d4fd1524155c0b
Jacob Hilker <jacob.hilker2@gmail.com>
2020-10-14 14:06:54 -0400

    added conditions feels like and current temp

--- .config/polybar/scripts/weather/wtr-bar.sh
+++ .config/polybar/scripts/weather/wtr-bar.sh
@@ -3,9 +3,14 @@
 # curl -sL 'https://wttr.in?format=1' ## for the weather symbol and the current temp
 # curl -sL 'https://wttr.in/?format=%t' ## for just the current temp
 #curl -sL 'http://wttr.in/?format=%t+(%f)' | sed 's/+//g' ## for current temp and feels like 
+# curl -sL 'https://wttr.in/?format=%t+(%f),%20%C&m' | sed 's/+//g' | xargs ##current temp, feels like, and condition
+
+
 
 # curl -sL 'https://wttr.in/?format=%t%20%C&m' ## for the current temp in Celsius and condition
 # curl -sL 'https://wttr.in?format=1&m' ## for the weather symbol and the current temp in Celsius
 # curl -sL 'https://wttr.in/?format=%t&m' ## for just the current temp in Celsius
 
-curl -sL 'http://wttr.in/?format=%t+(%f)&m' | sed 's/+//g' ## for current temp and feels like, in Celsius
+#curl -sL 'http://wttr.in/?format=%t+(%f)&m' | sed 's/+//g' | xargs ## for current temp and feels like, in Celsius
+
+curl -sL 'https://wttr.in/?format=%t+(%f),%20%C&m' | sed 's/+//g' | xargs ## current temp, feels like, condition in Celsius
