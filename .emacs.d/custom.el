f91d4b3a393393c2542674e8ca825ed90bd7d5df
Jacob Hilker <jacob.hilker2@gmail.com>
2020-10-26 20:53:31 -0400

    fixed todos

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -36,6 +36,7 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#073642" "#859900"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#073642" "#56697A"))
  '(jedi:complete-on-dot t)
+ '(line-spacing 0.1)
  '(nrepl-message-colors
    '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(objed-cursor-color "#dc322f")
@@ -50,7 +51,7 @@
  '(org-modules
    '(ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe ol-rmail org-tempo ol-w3m))
  '(package-selected-packages
-   '(idle-org-agenda bm emms org-sidebar helm-org org-chef tomatinho org-journal ox-rst ox-mediawiki yaml-mode tagedit web-mode lorem-ipsum flycheck xref-js2 js2-refactor js2-mode company mediawiki golden-ratio atom-one-dark-theme color-theme-sanityinc-solarized linum-relative treemacs-magit treemacs-projectile treemacs-evil treemacs evil-smartparens smartparens csv-mode yasnippet-snippets xresources-theme xresurces-theme zenburn-theme gruber-darker-theme gruvbox-theme ewal org-ql vimrc-mode wordgen smart-mode-line csv org-make-toc nav neotree slack toc-org org-super-agenda org-bullets bbcode-mode elpy auto-complete counsel ivy decide wordsmith-mode writeroom-mode writegood-mode wc-mode doom-modeline solarized-theme color-theme-solarized doom-themes restart-emacs yasnippet unfill general dashboard projectile all-the-icons magit which-key imenu-list evil-collection evil undo-tree use-package))
+   '(idle-org-agenda bm emms org-sidebar helm-org org-chef tomatinho org-journal ox-rst ox-mediawiki yaml-mode tagedit web-mode lorem-ipsum flycheck xref-js2 js2-refactor js2-mode company mediawiki golden-ratio atom-one-dark-theme color-theme-sanityinc-solarized linum-relative treemacs-magit treemacs-projectile treemacs-evil treemacs evil-smartparens smartparens csv-mode yasnippet-snippets xresources-theme xresurces-theme zenburn-theme gruber-darker-theme gruvbox-theme ewal org-ql vimrc-mode wordgen smart-mode-line csv org-make-toc nav neotree slack toc-org org-super-agenda org-bullets bbcode-mode elpy auto-complete counsel ivy decide wordsmith-mode writeroom-mode writegood-mode wc-mode doom-modeline solarized-theme color-theme-solarized restart-emacs yasnippet unfill general dashboard projectile all-the-icons magit which-key imenu-list evil-collection evil undo-tree use-package))
  '(pdf-view-midnight-colors (cons "#839496" "#002b36"))
  '(rustic-ansi-faces
    ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
