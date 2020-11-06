d3d2b492f5169471f78f93443b8c0887c25e3ab4
Jacob Hilker <jacob.hilker2@gmail.com>
2020-11-06 15:05:16 -0500

    fixed font

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -75,11 +75,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;'(default ((((class color) (min-colors 16777215)) (:background "#2e3440" :foreground "#fdf4c1" :family "Iosevka" :foundry "CYEL" :slant normal :weight normal :height 120 :width normal)) (((class color) (min-colors 255)) (:background "#262626" :foreground "#ffffaf" :family "Iosevka" :foundry "CYEL" :slant normal :weight normal :height 120 :width normal))))
  '(italic ((t (:underline nil :slant italic))))
  '(org-agenda-date ((t (:foreground "#d3869b" :underline nil :weight normal))))
  '(org-agenda-date-today ((t (:foreground "#83a598" :slant normal :weight bold))))
  '(org-agenda-date-weekend ((t (:foreground "#fabd2f" :underline nil :slant italic :weight light))))
  '(org-agenda-property-face ((t nil)))
  '(org-agenda-structure ((t (:foreground "#ebdbb2" :weight ultra-bold))))
+ '(org-hide ((t (:foreground "#282828"))))
  '(org-super-agenda-header ((t (:inherit org-agenda-structure :underline t)))))
