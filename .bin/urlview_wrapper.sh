34d8b8f89ee986af09f5a0000b9c4f86a03627ca
Jacob Hilker <jacob.hilker2@gmail.com>
2020-12-17 22:46:58 -0500

    fixed issue with mpv, added more podcasts to feeds

--- .bin/urlview_wrapper.sh
+++ .bin/urlview_wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ext="${1##*.}"
-mpvFiles="mkv mp4 avi mov wmv flv gif"
+mpvFiles="mkv mp4 avi mov wmv flv gif mp3"
 fehFiles="png jpg jpeg jpe"
-wgetFiles="mp3 flac opus mp3?source=feed pdf"
+wgetFiles="flac opus mp3?source=feed pdf"
 
 if echo $fehFiles | grep -w $ext > /dev/null; then
 	nohup swallow gpicview "$1" >/dev/null &
 elif echo $mpvFiles | grep -w $ext > /dev/null; then
-	nohup swallow mpv --loop --quiet "$1" > /dev/null &
+	nohup alacritty -e swallow mpv --loop --quiet --force-window='immediate' "$1" > /dev/null &
 elif echo $wgetFiles | grep -w $ext > /dev/null; then
 	nohup wget "$1" >/dev/null &
 elif echo $1 | grep youtube > /dev/null; then
@@ -19,3 +19,4 @@ elif echo $1 | grep vimeo > /dev/null; then
 else
 	nohup firefox "$1" >/dev/null &
 fi
+
