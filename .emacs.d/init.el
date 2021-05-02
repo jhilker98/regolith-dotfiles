1c3082c57ee37988346ef46ef1ccfb3cd3a58c66
Jacob Hilker <jacob.hilker2@gmail.com>
2021-05-02 17:39:34 -0400

    added new jmacs, deleted custom.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file "~/.emacs.d/jmacs.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key toc-org ivy general dashboard org-tempo doom-modeline org-plus-contrib evil-surround evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
