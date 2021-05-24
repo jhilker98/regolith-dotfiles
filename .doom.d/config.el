b7dfed2543f2a9a9bca3ff4670b9aa355b953712
Jacob Hilker <jacob.hilker2@gmail.com>
2021-05-24 19:39:12 -0400

    restarting doom emacs org mode section

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,7 +1,7 @@
 (setq user-full-name "Jacob Hilker"
       user-mail-address "jacob.hilker2@gmail.com")
 
-  (setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-gruvbox)
 
 (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 14)
       doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 14))
@@ -26,30 +26,17 @@
 
 (after! org
 (setq
-
- org-directory "~/Dropbox/org/"
- org-agenda-files '("~/Dropbox/org/agenda.org")
- org-ellipsis " ▼ "
- org-deadline-warning-days 0
- org-todo-keywords '(
-      (sequence "TODO(t)" "INPR(i)" "NEXT(n)" "MEET(m)" "|" "DONE(d)" "CANCELED(c)")
-      (type "PROJ(p)")
-      (type "BLOG(b)"))
- org-todo-keyword-faces
-    '(("TODO" . (:foreground "#cc241d" :weight black :underline t))
-
-   ("INPR" . (:foreground "#fe8019" :weight bold))
-    ("NEXT" . (:foreground "#fabd2f" :weight black))
-    ("MEET" . (:foreground "#b8bb26" :weight bold))
-   ("PROJ" . (:foreground "#458588" :slant italic :weight bold))
-   ("BLOG" . (:foreground "#83a598"))
-   ("SOMEDAY" . (:foreground "#d3869b" :slant italic))
-   ("DONE" . (:foreground "#83a598" :strike-through t))
-   ("CANCELED" . (:foreground "cc241d" :strike-through t)))
-
- )
-
-(add-hook 'org-mode-hook 'mixed-pitch-mode)
+      org-directory "~/Dropbox/org"
+      org-agenda-files '("~/Dropbox/org/agenda.org" "~/Dropbox/org/projects.org" "~/Dropbox/org/contacts.org" "~/Dropbox/org/todo.org")
+      org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+                          (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))
+      org-todo-keyword-faces '(
+                               ("TODO" . (:foreground "#cc241d" :weight bold :underline t))
+                               ("NEXT" . (:foreground "#fe8019" :weight bold))
+                               ("DONE" . (:foreground "#83a598" :weight bold :strike-through t))
+                               ("WAITING" . (:foreground "#d3869b" :weight bold :slant italic))
+                               ("CANCELLED" . (:foreground "#83a598" :weight bold :strike-through t))
+                               ))
 )
 
 (use-package! org-super-agenda
@@ -58,85 +45,6 @@
   (require 'evil-org-agenda)
   :config
   (org-super-agenda-mode))
-
-(after! org-super-agenda
-  (add-to-list 'org-agenda-custom-commands
-    '("p" "Planner"
-      (
-        (agenda "" ((org-agenda-span 'day)
-                    (org-agenda-use-time-grid t)
-                    (org-agenda-time-grid '((daily today weekly require-timed)()() "" nil))
-		              (org-agenda-show-all-dates t)
-			          (org-agenda-compact-blocks nil)
-			          (org-agenda-overriding-header "")
-			          (org-agenda-start-day nil)
-                    (org-super-agenda-groups
-                    '((:name "Habits"
-                       :habit t
-                       :category "HABIT"
-                       :order 1)
-
-                      (:name "Chores"
-                       :habit t
-                       :category "CHORE"
-                       :order 2)
-
-                      (:name none
-                       :discard (:todo "DONE")
-		                 :time-grid t
-				         :date today
-				         :scheduled today
-				         :deadline today
-				         :order 1)))))
-                    (agenda "" ((org-agenda-overriding-header "Coming Up Soon")
-                                (org-agenda-time-grid '((daily today weekly require-timed)()() "" nil))
-                                (org-agenda-format-date "\n%d %b (%a.)")
-                                (org-agenda-start-day "+1d")
-                                (org-agenda-span 2)
-                                (org-super-agenda-groups
-                                 '((:name "Classwork and Meetings"
-                                    :category ("CPSC430" "DGST101" "CPSC414" "CPSC444" "MEETING")
-                                    :order 1)
-
-                                   (:name "Personal"
-                                    :category ("WRITING" "CAMPAIGN" "PERSONAL" "NANO" "UMWCLUB")
-                                    :order 2)
-
-                                   (:name "Overdue"
-                                    :deadline past
-                                    :scheduled past
-                                    :face '(:foreground "red")
-                                    :order 3)
-
-                                   (:name "Projects"
-                                    :todo "PROJECT"
-                                    :category "PROJECT"
-                                    :order 4)))))
-
-                                (alltodo "" ((org-agenda-overriding-header "To Do")
-                                             (org-agenda-skip-function '(org-agenda-skip-entry-if 'timestamp))
-                                             (org-agenda-sorting-strategy '(todo-state-up category-up ))
-                                             (org-super-agenda-groups
-                                              '(
-                                                (:name "Classwork and Meetings"
-                                                 :category ("CLAS204" "CPSC405" "CPSC419" "CPSC445" "MEETING" "CLASSES")
-                                                 :order 1)
-
-                                                (:name "Personal"
-                                                 :category ("WRITING" "CAMPAIGN" "PERSONAL" "NANO" "UMWCLUB")
-                                                 :order 2)
-
-                                                (:name "Projects"
-                                                 :category "PROJECT"
-                                                 :order 3)
-
-
-                                                (:discard (:category "HABIT"))
-                                                (:discard (:category "CHORE"))))))))))
-
-(defun jh/open-planner ()
-  (interactive)
-  (org-agenda nil "p"))
 
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e") ;; On Ubuntu
   (require 'mu4e);; on Ubuntu
@@ -180,5 +88,4 @@
 
 (map! :leader
       (:prefix ("o" . "open")
-       :desc "org agenda" "a" #'jh/open-planner
        :desc "News reader" "n" #'elfeed))
